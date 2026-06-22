be95beab231633b88a9019cb4d581fd241a6a66a
ilyaalwot <ghostreaper2765@gmail.com>
2026-06-22 18:29:11 +0300

    .

---
+++ BookYearComparator.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class BookYearComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book b1, Book b2) {
+        return Integer.compare(b1.getYear(), b2.getYear());
+    }
+}
